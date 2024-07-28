f747399e74b9abf01aeadc1a4b31b5700a496186
kiranrokade17 <rokadekiran533@gmail.com>
2024-07-28 12:00:28 +0530

    para heading created

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,8 @@ const About = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl text-center text-blue-600">About Us</h1>
+      <p>My name is kiran</p>
+      <h2>contact</h2>
     </div>
   );
 };

ff12e41d21c8685351ef680c30845aa2c6b0b1f2
Atharva262005 <atharvaralegankar2005@gmail.com>
2024-07-28 11:48:37 +0530

    changes in about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl text-center text-blue-600">About Us</h1>
+      <p className="text-lg text-gray-700">AquaSustain is a fictional company that specializes in water treatment and water quality monitoring. We are committed to providing clean water solutions to communities and industries around the world. Our team of experts is dedicated to ensuring that everyone has access to safe and clean water.</p>
     </div>
   );
 };
